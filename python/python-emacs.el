1b06f5588c07abfcdc37f2a2d0b7ddcc1ec3c836
Jarkko Saltiola <jarkko.saltiola@koodilehto.fi>
2015-09-21 15:49:51 +0300

    Sketching structure. Some aliases.

---
+++ python/python-emacs.el
@@ -0,0 +1,1 @@
+;; Python ELPY based IDE configuration
---
+++ python/python-shell.sh
@@ -0,0 +1,15 @@
+# Shell customizations for working with Python
+
+# Python package manager
+alias pipi="pip install"
+alias pips="pip search"
+alias pipr="pip uninstall"
+alias pipf="pip freeze | tee requirements.txt"
+alias pipfi="pip install -r requirements.txt"
+alias serve="python -m SimpleHTTPServer 3333"
+
+# Python Virtualenv
+alias va="source ./venv/bin/activate"
+alias vc="virtualenv venv"
+alias vd="deactivate"
+alias vp="pip install -r requirements.txt"
