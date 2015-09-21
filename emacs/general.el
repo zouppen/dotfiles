9218603cccc37218f9fd7c27edfee934069caf80
Jarkko Saltiola <jarkko.saltiola@koodilehto.fi>
2015-09-21 17:13:31 +0300

    Add emacs dir.

---
+++ emacs/general.el
@@ -0,0 +1,1 @@
+;;; TODO add major mode agnostic settings
---
+++ emacs/init.el
@@ -0,0 +1,5 @@
+;; Entry point for Emacs configurations.
+;; This file is symlinked into ~/.emacs.d/personal/init.el after Prelude install
+
+(load "~/konffit/emacs/prelude-personal/modes/acme-search.el")
+(load "~/konffit/emacs/prelude-personal/general.el")
