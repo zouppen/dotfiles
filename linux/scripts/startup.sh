09728e72219a8eb2a310c5321a3038f887df206e
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2016-01-25 21:51:04 +0200

    Run input config at startup & wakeup & udev event.

---
+++ linux/scripts/startup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# startup script for mint 17.3
+
+/home/jarkko/dotfiles/linux/bin/pledoff
+sudo /home/jarkko/dotfiles/linux/input-config.sh &
+
+if pgrep -f quicktile > /dev/null
+then
+    echo "Quicktile already running"
+else
+    quicktile --daemonize &
+fi
+
+if pgrep -f sxhkd > /dev/null
+then
+    echo "Sxhkd already running"
+    killall sxhkd && sxhkd &
+else
+    sxhkd &
+fi
+
+if pgrep -f thinkfan > /dev/null
+then
+    echo "Thinkfan already running"
+else
+    sudo thinkfan
+fi
