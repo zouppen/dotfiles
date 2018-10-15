1934d7d836f2ceec8ca50e8664739215a2d58a54
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2018-10-15 17:21:49 +0300

    Fix yasnippet. Re-add 'itr' ipdb debugger expansion snippet.

--- emacs/init.el
+++ emacs/init.el
@@ -2,19 +2,23 @@
 ;; See README for installation guide
 ;; This file is symlinked into ~/.emacs.d/personal/init.el
 
+
+
+
 (setq dotfiles-folder "~/dotfiles/")
 
 (setq ad-redefinition-action 'accept)
 
 ;; TODO dynamic loading?
 (let ((conf-files
-       '("emacs/modes/acme-search.el"
-         "emacs/modes/google-translate-mode.el"
+       '(
+         ;"emacs/modes/acme-search.el"
+ ;        "emacs/modes/google-translate-mode.el"
          "emacs/general.el"
          "emacs/programming.el"
          "emacs/orgmode.el"
-         "lang/web/web.el"
-         "lang/python/python.el"
+         ;"lang/web/web.el"
+         ;"lang/python/python.el"
          ;;"lang/clojure/clojure.el"
 )))
   (dolist (entry conf-files) (load (concat dotfiles-folder entry))))
